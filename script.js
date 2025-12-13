e15c8ed483a089db8bc9442e74e048fd3b6e99d3
junhosonmania <junhoson.mania@gmail.com>
2025-12-13 15:54:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let data = [
-    // [현재] Mania Seoul (구분: 'mania')
+    // [현재] mania seoul (구분: 'mania')
+    ['2026/04/26', '???', '???', 'SSMD', 'Promoter', 'mania'],
+    ['2026/03/29', '???', '???', '1975 Theater', 'Promoter', 'mania'],
+    ['2026/01/24', 'bar italia + bongjeingan live n seoul', 'bar italia, bongjeingan', 'Musinsa Garage', 'Promoter', 'mania'],
+    ['2026/01/18', 'Wisp Live In Seoul', 'Wisp', 'Musinsa Garage', 'Promoter', 'mania'],
+    ['2025/10/17', 'Pearl & The Oysters Live in Seoul', 'Pearl & The Oysters', 'West Bridge Live Hall', 'Promoter', 'mania'],
     ['2025/06/06', 'bed live in seoul', 'bed', 'Veloso Hongdae', 'Promoter', 'mania'],
     ['2025/03/03', 'HMLTD 포원스인마랖 IN SEOUL', 'HMLTD', 'Musinsa Garage', 'Promoter', 'mania'],
 
